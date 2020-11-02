c15f2ec30f3ed4b478c0b7a4e891b957f102dbe1
daxoliveira <dax-oliveira-br@gmail.com>
2020-11-02 17:02:37 -0600

    Create questionReducer.js

---
+++ src/reducers/questionReducer.js
@@ -0,0 +1,12 @@
+import { SHUFFLE_QUESTIONS } from '../actions/types';
+
+const initialState = {
+  questions: [],
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
