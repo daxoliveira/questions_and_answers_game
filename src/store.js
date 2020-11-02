92aaaa705f9a5d573fb00f0eaf1cae72fcf26b5a
daxoliveira <dax-oliveira-br@gmail.com>
2020-11-02 14:06:33 -0600

    Create store.js small fix

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer, 
   initialState, 
-  applyMiddleware(...applyMiddleware)
+  applyMiddleware(...middleware)
 );
 
 export default store;
